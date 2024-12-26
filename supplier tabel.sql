064eb0beb1216f4891f0b3fdae1b9a039f1bdab0
ynawinraaj <gomsyuva19@gmail.com>
2024-12-26 20:58:35 +0530

    Add files via upload

---
+++ supplier tabel.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Salesmans(
+
+ Salesman_id INT PRIMARY KEY,
+
+ name TEXT,
+
+ city TEXT,
+
+ Comission TEXT
+
+ );
+
+INSERT INTO Salesmans(Salesman_id,name,city,Comission)VALUES
+
+("5001","James Hoog",'New York','0.15'),
+
+("5002","Nail Knite","Paris","0.13"),
+
+("5005","Pit Alex","London","0.11"),
+
+("5006","Mc Lyon","Paris","0.14"),
+
+("5007","Paul Adam","Rome","0.13"),
+
+("5003","Lauson Hen","San Jose","0.12");
